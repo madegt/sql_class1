59fc0b97fc27fff7c8eec8dfe4e22a6d59b29af1
Madeleine Garbin <madeleine.garbin@kellogg.northwestern.edu>
2024-01-10 15:40:04 +0000

    solution

--- lab-1-2.sql
+++ lab-1-2.sql
@@ -13,5 +13,5 @@
 -- | 1906 | Chicago Cubs                      | 116  |
 -- | 2001 | Seattle Mariners                  | 116  |
 -- +------+-----------------------------------+------+
-
+select year, name, wins from teams order by wins asc;
 
--- lab-1-3.sql
+++ lab-1-3.sql
@@ -2,10 +2,11 @@
 
 -- Expected result:
 --
--- +------+----------------------+------+--------+
+-- +------+----------------------+--w----+--L------+
 -- | 1899 | Cleveland Spiders    | 20   | 134    |
 -- | 1962 | New York Mets.       | 40   | 120    |
 -- | 2003 | Detroit Tigers       | 43   | 119    |
 -- +------+----------------------+------+--------+
 
+Select year, name, wins, losses from teams order by losses desc limit 3;
 
