043a4b098f0264a9ebc259c57783cf563347bcb7
Madeleine Garbin <madeleine.garbin@kellogg.northwestern.edu>
2024-01-21 22:19:21 +0000

    class

--- school-2-drop-and-create.sql
+++ school-2-drop-and-create.sql
@@ -6,6 +6,6 @@ DROP TABle students;
 CREATE TABLE students (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   first_name TEXT,
-  last_name TEXT
+  last_name TEXT,
   phone TEXT
 );
---
+++ school-5-insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO students 
+(first_name, 
+last_name, 
+phone 
+)
+values (
+    'Ben',
+    'Block',
+    '111-2222-3333');
